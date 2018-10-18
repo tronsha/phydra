34576c6bec61b400b3d9871bc91e9dad2b693eef
Stefan Hüsges <tronsha@gmx.de>
2018-10-18 12:09:47 +0200

    Update

--- app.py
+++ app.py
@@ -1,42 +1,54 @@
-import socket, time, string, _thread 
+import socket
+import time
+import _thread
+import sys
+
 
 def irc():
-    ircConnection = connect()
+    irc_connection = connect()
     while True:
         try:
-            receivedData = ircConnection.recv(4096)
-            if not receivedData: break
-            receivedText = receivedData.decode("utf-8").strip()
-            if receivedText is not '': 
-                print(receivedText)
-                splittedText = receivedText.split(' ', 1)
-                if splittedText[0] == 'PING':
-                    submitText = "PONG %s\n" % splittedText[1]
-                    ircConnection.send(submitText.encode())
-                    print(submitText.strip())
+            received_data = irc_connection.recv(4096)
+            if not received_data:
+                break
+            received_text = received_data.decode("utf-8").strip()
+            if received_text is not '':
+                print(received_text)
+                split_text = received_text.split(' ', 1)
+                if split_text[0] == 'PING':
+                    submit_text = "PONG %s\n" % split_text[1]
+                    irc_connection.send(submit_text.encode())
+                    print(submit_text.strip())
             time.sleep(.1)
         except socket.timeout:
-            ircConnection = connect()
+            break
+        except ConnectionResetError:
+            break
+    sys.exit()
+
 
 def connect():
     try:
-        ircConnection = socket.create_connection(('chat.freenode.net', 6667), 600)
-    except:
+        irc_connection = socket.create_connection(('chat.freenode.net', 6667), 600)
+    except socket.error:
         return False
-    ircConnection.send(b"USER Phydra * * :Phydra\n")
-    ircConnection.send(b"NICK Phydra\n")
-    return ircConnection
+    irc_connection.send(b"USER Phydra * * :Phydra\n")
+    irc_connection.send(b"NICK Phydra\n")
+    return irc_connection
+
 
 def setup():
-    _thread.start_new_thread(irc)
+    _thread.start_new_thread(irc, ())
+
 
 def loop():
     time.sleep(1)
+
 
 def main():
     setup()
     while True:
         loop()
-    return True
+
 
 main()
